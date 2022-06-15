a595cd658f49ccdb07242b6655c6e0f4b9c8b37c
surya <surya.git@gmail.com>
2022-06-15 15:54:47 +0530

    file upload

--- models/movie.js
+++ models/movie.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const path = require('path')
-const coverImageBasePath = 'uploads/movieCovers'
 
 const movieSchema = new mongoose.Schema({
    title : {
@@ -23,9 +21,13 @@ const movieSchema = new mongoose.Schema({
       required : true,
       default : Date.now
    },
-   coverImageName : {
-      type : String,
+   coverImage : {
+      type : Buffer,
       required : true
+   },
+   coverImageType :{
+       type : String,
+       required : true
    },
    director : {
       type : mongoose.Schema.Types.ObjectId,
@@ -34,11 +36,16 @@ const movieSchema = new mongoose.Schema({
    }
 })
 
-movieSchema.virtual('coverImagePath').get(function(){
-   if(this.coverImageName != null){
-      return path.join('/', coverImageBasePath, this.coverImageName)
+/* movieSchema.virtual('coverImagePath').get(function(){
+   if(this.coverImage != null && this.coverImageType != null){
+      return `data : ${this.coverImageType};charset=utf-8;base64
+      ${this.coverImage.toString('base64')}`
    }
-})
+}) */
+movieSchema.virtual('coverImagePath').get(function() {
+   if (this.coverImage != null && this.coverImageType != null) {
+     return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+   }
+ })
  
 module.exports = mongoose.model('Movie', movieSchema)
-module.exports.coverImageBasePath = coverImageBasePath
--- routes/movies.js
+++ routes/movies.js
@@ -1,41 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
 const Movie = require('../models/movie')
-const uploadPath = path.join('public', Movie.coverImageBasePath)
 const Director = require('../models/director')
-
-const imageMimeTypes = ['image/jpeg','image/jpg','image/png','image/gif']
-
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-})
+const imageMimeTypes = ['image/jpg', 'image/jpeg', 'image.png', 'image/gif']
 
 //all movies route
 router.get('/', async (req, res) => {
     let query = Movie.find()
-    if(req.query.title != null && req.query.title !=''){
-        query= query.regex('title',new RegExp(req.query.title ,'i') )
+    if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
     if (req.query.releasedBefore != null && req.query.releasedBefore != '') {
         query = query.lte('releaseDate', req.query.releasedBefore)
-      }
+    }
     if (req.query.releasedAfter != null && req.query.releasedAfter != '') {
         query = query.gte('releaseDate', req.query.releasedAfter)
     }
-    try{
+    try {
         const movies = await query.exec()
-        res.render('movies/index',{
-            movies : movies,
-            searchOptions :req.query
+        res.render('movies/index', {
+            movies: movies,
+            searchOptions: req.query
         })
-    }catch{
-         res.redirect('/')
+    }
+    catch {
+        res.redirect('/')
     }
 })
 
@@ -45,35 +34,28 @@ router.get('/new', async (req, res) => {
 })
 
 //create movie route
-router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+router.post('/', async (req, res) => {
+
     const movie = new Movie({
-        title : req.body.title,
-        director : req.body.director,
-        releaseDate : new Date(req.body.releaseDate),
-        runTime : req.body.runTime,
-        coverImageName : fileName,
-        description : req.body.description
+        title: req.body.title,
+        director: req.body.director,
+        releaseDate: new Date(req.body.releaseDate),
+        runTime: req.body.runTime,
+        description: req.body.description
+
     })
+    saveCover(movie, req.body.cover)
     try {
-
         const newMovie = await movie.save()
-
         res.redirect(`movies`)
         console.log('submitted')
         /* res.redirect(`movies/${newMovie.id}`) */
-    } catch (err){
-        if(movie.coverImageName != null){
-          removeBookCover(movie.coverImageName)
-        } 
+    } catch (err) {
+        console.log(err)
         renderNewPage(res, movie, true)
     }
 })
-function removeBookCover(fileName){
-   fs.unlink(path.join(uploadPath,fileName), err =>{
-       if(err) console.error(err)
-   })
-} 
+
 async function renderNewPage(res, movie, hasError = false) {
     try {
         const directors = await Director.find({})
@@ -87,6 +69,14 @@ async function renderNewPage(res, movie, hasError = false) {
     }
     catch {
         res.redirect('/movies')
+    }
+}
+function saveCover(movie, coverEncoded) {
+    if (coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+        movie.coverImage = new Buffer.from(cover.data, 'base64')
+        movie.coverImageType = cover.type
     }
 }
 
